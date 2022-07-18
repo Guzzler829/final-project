d1eb3d396ecba0dc76a31245191f6145b64f5758
Gus Johnson <nyanpasu@charter.net>
2022-07-17 19:45:25 -0500

    Added image posts and fixed a bunch of stuff

--- src/components/input-field.js
+++ src/components/input-field.js
@@ -10,11 +10,14 @@ export default class InputField extends React.Component {
     constructor(props) {
         super(props);
         this.state= {
-            redirect: <></>
+            redirect: <></>,
+            textPost: true
         };
         this.submitPost = this.submitPost.bind(this);
+        this.switchInput = this.switchInput.bind(this);
         this.theTitle = React.createRef();
         this.theText = React.createRef();
+        this.theURL = React.createRef();
     }
 
     submitPost() {
@@ -24,29 +27,71 @@ export default class InputField extends React.Component {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
+                type: (this.state.textPost ? "img" : "text"),
                 title: this.theTitle.current.value,
-                text: this.theText.current.value
+                text: this.theText.current.value,
+                url: this.theURL.current.value
             })
         });
         this.setState({redirect: <Redirect to="/" />});
     }
 
+    switchInput() {
+        if(this.state.textPost) {
+            this.setState({textPost: false});
+        } else {
+            this.setState({textPost: true});
+        }
+        console.log(this.state.textPost)
+    }
+
     render(){
-        return (
-            <div className='input-field'>
-                <Form>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control ref={this.theTitle} placeholder="An interesting title" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                        <Form.Label >Text</Form.Label>
-                        <Form.Control ref={this.theText} as="textarea" placeholder="..." rows={3} />
-                    </Form.Group>
-                </Form>
-                <Button onClick={this.submitPost}>Submit</Button>
-                {this.state.redirect}
-            </div>
-        );
+        if(this.state.textPost){
+            return (
+                <div className='input-field'>
+                    <div className='input-buttons'>
+                        <button onClick={this.switchInput} className={this.state.textPost ? 'input-button' : 'input-button is-active'}>Text</button>
+                        <button onClick={this.switchInput} className={this.state.textPost ? 'input-button is-active' : 'input-button'}>Image</button>
+                    </div>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control ref={this.theTitle} placeholder="An interesting title" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label >Image URL</Form.Label>
+                            <Form.Control ref={this.theURL} as="textarea" placeholder="https://..." rows={3} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label >Text</Form.Label>
+                            <Form.Control ref={this.theText} as="textarea" placeholder="..." rows={3} />
+                        </Form.Group>
+                    </Form>
+                    <Button onClick={this.submitPost}>Submit</Button>
+                    {this.state.redirect}
+                </div>
+            );
+        } else {
+            return (
+                <div className='input-field'>
+                    <div className='input-buttons'>
+                        <button onClick={this.switchInput} className={this.state.textPost ? 'input-button' : 'input-button is-active'}>Text</button>
+                        <button onClick={this.switchInput} className={this.state.textPost ? 'input-button is-active' : 'input-button'}>Image</button>
+                    </div>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control ref={this.theTitle} placeholder="An interesting title" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label >Text</Form.Label>
+                            <Form.Control ref={this.theText} as="textarea" placeholder="..." rows={3} />
+                        </Form.Group>
+                    </Form>
+                    <Button onClick={this.submitPost}>Submit</Button>
+                    {this.state.redirect}
+                </div>
+            );
+        }
     }
 }
--- src/components/post.js
+++ src/components/post.js
@@ -1,20 +1,52 @@
+import { useEffect, useRef } from 'react';
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup"
 
+const apiURL = 'https://crudcrud.com/api/91ba9e9f61e541de92c8e6d9b14465a5';
+
 export default function Post(props) {
-    if(props.type == "image"){
-        return (
-            <div className="post">
-                <h2>{props.title}</h2>
-                <img src={props.imgSrc} />
-                <div className="post-footer">
-                    <ButtonGroup>
-                        <Button variant="secondary">Commments</Button>
-                        <Button variant="secondary">Share</Button>
-                    </ButtonGroup>
+
+    const theImage = useRef(null);
+
+    function deletePost() {
+        fetch(apiURL + '/posts/' + props.postId, { method: 'DELETE' });
+    }
+
+    function toggleActive() {
+        theImage.current.classList.toggle("is-active");
+    }
+
+    switch(props.typeOfPost) {
+        case "img":
+            return (
+                <div className="post">
+                    <h2>{props.title}</h2>
+                    <img src={props.imgSrc} onClick={toggleActive} ref={theImage} id="image-element" />
+                    <div className="post-footer">
+                    <p>{props.text}</p>
+                        <ButtonGroup>
+                            <Button variant="secondary">Commments</Button>
+                            <Button variant="secondary">Share</Button>
+                            <Button variant="danger" onClick={deletePost}>Delete</Button>
+                        </ButtonGroup>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        case "link":
+            return (
+                <div className="post">
+                    <a href={props.myHref}><h2>{props.title}</h2></a>
+                    <p>{props.text}</p>
+                    <div className="post-footer">
+                        <ButtonGroup>
+                            <Button variant="secondary">Commments</Button>
+                            <Button variant="secondary">Share</Button>
+                            <Button variant="danger" onClick={deletePost}>Delete</Button>
+                        </ButtonGroup>
+                    </div>
+                </div>
+            )
+        
     }
     return (
         <div className="post">
@@ -24,6 +56,7 @@ export default function Post(props) {
                 <ButtonGroup>
                     <Button variant="secondary">Commments</Button>
                     <Button variant="secondary">Share</Button>
+                    <Button variant="danger" onClick={deletePost}>Delete</Button>
                 </ButtonGroup>
             </div>
         </div>
